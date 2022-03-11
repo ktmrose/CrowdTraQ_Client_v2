c7001c9d5da35155216b2a4a282d3c7c55dcd1cf
ktmrose <ktm.rose@yahoo.com>
2022-03-10 21:19:42 -0700

    changes related to node and npm upgrade

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import logo from './logo.svg';
 import React, { useState } from 'react';
 import './App.css';
-import Dashboard from './components/Dashboard'
+import Dashboard from './components/Dashboard';
+// import fontawesome from '~@fortawesome/fontawesome';
+// import FontAwesomeIcon from '~@fortawesome/react-fontawesome';
+// import { faCoffee } from '~@fortawesome/fontawesome-free'
 
 function App() {
   // const [accessToken, setAccessToken] = useState("testToken"); //used to connect to server
+
+  // fontawesome.library.add("faCoffee")
   return (
     <div className="App background-img">
         <img src={logo} className="App-logo" alt="CrowdTraQ logo" />
         <Dashboard
           // accessToken={accessToken}
         />
+        {/* <FontAwesomeIcon icon="faCoffee" /> */}
     </div>
   );
 }
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Dashboard.css';
 
 const Dashboard = (props) => { //props.accessToken to make requests to server
+    const [tokens, setTokens] = useState(12);
     return (
-        <div>
-            Hello, this is your access token: 
+        <div className="container">
+            <p>Tokens: {tokens}</p>
+            <div className="icon-container">
+                {/* <FontAwesomeIcon icon="fa-solid fa-fire" /> */}
+            </div>
         </div>
     )
 }
