15e8c3ce5b35f4ef3a4b09d8762a11537017abb4
ktmrose <ktm.rose@yahoo.com>
2022-03-10 19:00:15 -0700

    starting on dashboard

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import logo from './logo.svg';
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Dashboard from './components/Dashboard'
 
 function App() {
+  // const [accessToken, setAccessToken] = useState("testToken"); //used to connect to server
   return (
     <div className="App background-img">
-      {/* <header className="App-header"> */}
         <img src={logo} className="App-logo" alt="CrowdTraQ logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Taco meat!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      {/* </header> */}
+        <Dashboard
+          // accessToken={accessToken}
+        />
     </div>
   );
 }
---
+++ src/components/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = (props) => { //props.accessToken to make requests to server
+    return (
+        <div>
+            Hello, this is your access token: 
+        </div>
+    )
+}
+
+export default Dashboard;
