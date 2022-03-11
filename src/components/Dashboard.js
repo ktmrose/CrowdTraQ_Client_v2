4678da2d3ebdae192ff8bd26b1567a6f8719c045
ktmrose <ktm.rose@yahoo.com>
2022-03-11 16:15:24 -0700

    responsive dashboard ui

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,31 @@
 import React, { useState } from 'react';
 import './Dashboard.css';
+import { faFire, faBan } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Dashboard = (props) => { //props.accessToken to make requests to server
-    const [tokens, setTokens] = useState(12);
+    const [tokens, setTokens] = useState(12); //TODO: set from server
+    const [currentSong, setCurrentSong] = useState("{Current Song}"); //TODO: set from server
+    const [currentArtist, setCurrentArtist] = useState("{Current Artist}"); //TODO: set from server
+
+    //TODO: addTokens upon Websocket state push.
+
     return (
         <div className="container">
-            <p>Tokens: {tokens}</p>
+            <h4>Tokens: {tokens}</h4>
+            <h1>How do you like {currentSong} by {currentArtist} ?</h1>
             <div className="icon-container">
-                {/* <FontAwesomeIcon icon="fa-solid fa-fire" /> */}
+                <FontAwesomeIcon className="reaction-icon" icon={faFire} />
+                <FontAwesomeIcon className="reaction-icon" icon={faBan} />
+                {/* <div className="icon-text">
+                    <span>HOT!</span>
+                    <span>NOT!</span>
+                </div> */}
+            </div>
+            <div className="btn-container">
+                <button className="btn">
+                    Add Song
+                </button>
             </div>
         </div>
     )
