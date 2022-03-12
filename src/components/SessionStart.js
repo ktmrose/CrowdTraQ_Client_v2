e2bf2ac4939e3768c60189ead927538402e2493b
ktmrose <ktm.rose@yahoo.com>
2022-03-11 21:30:17 -0700

    room code form

--- src/components/SessionStart.js
+++ src/components/SessionStart.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const SessionStart = (props) => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const onFormSubmit = (data) => {
+        console.log(data);
+        //TODO: send data to server
+    }
+
 
     return (
         <div className="container">
             <h1>Enter room code:</h1>
-            <input 
-                type="text"
-                required={true}
-            />
-            <button className="btn">Enter</button>
+            <form onSubmit={handleSubmit(onFormSubmit)}>
+                <input 
+                    type="text"
+                    name="room-code"
+                    {...register("room-code")}
+                    pattern="^[a-zA-Z0-9]+$"
+                    title="Numbers and letters only"
+                />
+                <button className="btn">Enter</button>
+            </form>
         </div>
     )
 }
